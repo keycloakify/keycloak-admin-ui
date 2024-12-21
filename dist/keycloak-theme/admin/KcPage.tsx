b86ad8e8a5fa03747b20ccbe5b4b6c3caa567975
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-12-21 21:01:01 +0100

    update dist

--- dist/keycloak-theme/admin/KcPage.tsx
+++ dist/keycloak-theme/admin/KcPage.tsx
@@ -1,6 +1,6 @@
 import { lazy } from "react";
 import { KcAdminUiLoader } from "@keycloakify/keycloak-admin-ui";
-import { KcContext, setKcContext } from "./KcContext";
+import { KcContext } from "./KcContext";
 
 const KcAdminUi = lazy(() => import("./KcAdminUi"));
 
