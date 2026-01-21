71ce89b0f1a9c606bb72328c4fd09a46594b4585
garronej <joseph.garrone@protonmail.com>
2026-01-21 03:31:15 +0100

    Fix testing script

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,16 +12,17 @@ yarn
 
 npx keycloakify initialize-admin-theme
 
-rm -r node_modules/@keycloakify/keycloak-admin-ui
-cp -r ../dist node_modules/@keycloakify/keycloak-admin-ui
-rm -r node_modules/.cache
+# Point the dependency to the local build so postinstall uses it.
 node -e "\
     const fs = require('fs');\
-    const pj_path= './node_modules/@keycloakify/keycloak-admin-ui/package.json';
-    const pj = JSON.parse(fs.readFileSync(pj_path, 'utf8'));\
-    pj.version = JSON.parse(fs.readFileSync('package.json', 'utf8')).dependencies['@keycloakify/keycloak-admin-ui'].slice(1);\
-    fs.writeFileSync(pj_path, Buffer.from(JSON.stringify(pj, null, 2), 'utf8'));\
+    const pjPath = './package.json';\
+    const pj = JSON.parse(fs.readFileSync(pjPath, 'utf8'));\
+    pj.dependencies = pj.dependencies || {};\
+    pj.dependencies['@keycloakify/keycloak-admin-ui'] = 'file:../dist';\
+    fs.writeFileSync(pjPath, Buffer.from(JSON.stringify(pj, null, 2), 'utf8'));\
 "
+
+yarn install
 
 yarn postinstall
 
